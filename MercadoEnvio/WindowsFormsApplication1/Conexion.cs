b7b6f63bdb264f6d03d5ab24d35634a54f45c46e
demiandominguez <demianadominguez@hotmail.com>
2016-05-31 17:24:52 -0300

    Agregada clase conexio

---
+++ MercadoEnvio/WindowsFormsApplication1/Conexion.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    class Conexion
+    {
+        private string source;
+        private string nombreInstancia;
+        private string nombreBD;
+        private string user;
+        private string password;
+        private SqlConnection conn;
+        private string p1;
+        private string p2;
+        private string p3;
+        private string p4;
+        private string p5;
+
+        public Conexion(String _source, String _nombreInstancia, String _nombreBD, String _user, String _password)
+        {
+            this.source = _source;
+            this.nombreInstancia = _nombreInstancia;
+            this.nombreBD = _nombreBD;
+            this.user = _user;
+            this.password = _password;
+        }
+
+        public void conectar(){
+            this.conn = new SqlConnection();
+            conn.ConnectionString = this.conecctionString();
+        }
+            
+        public String conecctionString(){
+            return    "Data Source=" + this.source + "\\" + this.nombreInstancia
+                       + ";Initial Catalog=" + this.nombreBD
+                       + ";User ID=" + this.user
+                       + ";Password=" + this.password
+                       + ";Trusted_Connection=False;";
+        }   
+        
+        public SqlConnection getConexion(){
+            try
+            {
+                this.conn.Open();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("No se pudo establecer la conexión con la Base de datos."); 
+                Application.Exit();
+            }
+            return this.conn;
+        }
+    }
+}
--- MercadoEnvio/WindowsFormsApplication1/Login y Seguridad/Ingresar.cs
+++ MercadoEnvio/WindowsFormsApplication1/Login y Seguridad/Ingresar.cs
@@ -13,31 +13,25 @@ namespace WindowsFormsApplication1
 {
     public partial class frmIngresar : Form
     {
-        SqlConnection con = new SqlConnection();
+        Conexion conn;
 
         public frmIngresar()
         {
             InitializeComponent();
 
-            string datosConexion = "Data Source=localhost" + "\\" + "SQLSERVER2012;Initial Catalog=GD1C2016;User ID=gd;Password=gd2016;Trusted_Connection=False;";
-            con.ConnectionString = datosConexion;
-            try
-            {
-                con.Open();
-            }
-            catch (Exception)
-            {
-                MessageBox.Show("No se pudo establecer la conexión con la Base de datos." + datosConexion); 
-                Application.Exit();
-            }
+            this.conn = new Conexion("localhost",
+                                      "SQLSERVER2012",
+                                      "GD1C2016",
+                                      "gd",
+                                      "gd2016");
+            conn.conectar();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             string textoCmd = "SELECT p.id FROM ADIOS_TERCER_ANIO.Usuario p where p.usuario ='" + txtUsr.Text + "' and p.pass='" + txtContra.Text + "'";
 
-            SqlCommand cmd = new SqlCommand(textoCmd, con);
-
+            SqlCommand cmd = new SqlCommand(textoCmd, conn.getConexion());
             SqlDataReader dr = cmd.ExecuteReader();
             if (dr.Read())
             {
@@ -45,7 +39,7 @@ namespace WindowsFormsApplication1
                 //MessageBox.Show(idUsuario.ToString());
                 dr.Close();
                 textoCmd = "SELECT idRol FROM ADIOS_TERCER_ANIO.RolUsuario WHERE idUsuario = " + idUsuario;
-                cmd = new SqlCommand(textoCmd, con);
+                cmd = new SqlCommand(textoCmd, conn.getConexion());
                 dr = cmd.ExecuteReader();
 
                 dr.Read();
@@ -65,7 +59,7 @@ namespace WindowsFormsApplication1
                 } else {
                     dr.Close();
                     textoCmd = "SELECT r.nombre FROM ADIOS_TERCER_ANIO.RolUsuario ru inner join ADIOS_TERCER_ANIO.Rol r on r.id = ru.idRol WHERE idUsuario = " +idUsuario;
-                    cmd = new SqlCommand(textoCmd, con);
+                    cmd = new SqlCommand(textoCmd, conn.getConexion());
                     dr = cmd.ExecuteReader();
                     dr.Read();
                     string roles = dr.GetString(0);
