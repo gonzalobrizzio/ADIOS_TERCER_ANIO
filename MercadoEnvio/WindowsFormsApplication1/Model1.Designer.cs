83a9136e5d2ea495273bf540ad4be73f4223e27d
demiandominguez <demianadominguez@hotmail.com>
2016-06-04 17:05:30 -0300

    Corregido framework y rrores por ese tema

--- MercadoEnvio/WindowsFormsApplication1/Model1.Designer.cs
+++
@@ -1,4 +0,0 @@
-﻿// La generación de código predeterminada está deshabilitada para el modelo 'E:\WorkSpace\ADIOS_TERCER_ANIO\MercadoEnvio\WindowsFormsApplication1\Model1.edmx'. 
-// Para habilitar la generación de código predeterminada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
-// por otro valor. Esta propiedad está disponible en la ventana Propiedades cuando se abre
-// el modelo en el diseñador.
