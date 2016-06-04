c9162a0a74582d4a779bdce06926968e5a26648b
demiandominguez <demianadominguez@hotmail.com>
2016-06-04 17:31:39 -0300

    Interfaz modif rol lista

--- MercadoEnvio/WindowsFormsApplication1/ABM Rol/ModificarRoles.Designer.cs
+++ MercadoEnvio/WindowsFormsApplication1/ABM Rol/ModificarRoles.Designer.cs
@@ -39,8 +39,8 @@
             this.btnAgregar = new System.Windows.Forms.Button();
             this.lblFuncionalidadesTotales = new System.Windows.Forms.Label();
             this.lblFuncionalidadesActuales = new System.Windows.Forms.Label();
-            this.btnModificar = new System.Windows.Forms.Button();
             this.btnVolver = new System.Windows.Forms.Button();
+            this.btnModficar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvFuncionalidadesDisponibles)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvFuncionalidadesActuales)).BeginInit();
             this.SuspendLayout();
@@ -48,7 +48,7 @@
             // lblNuevoNombre
             // 
             this.lblNuevoNombre.AutoSize = true;
-            this.lblNuevoNombre.Location = new System.Drawing.Point(12, 50);
+            this.lblNuevoNombre.Location = new System.Drawing.Point(14, 10);
             this.lblNuevoNombre.Name = "lblNuevoNombre";
             this.lblNuevoNombre.Size = new System.Drawing.Size(131, 13);
             this.lblNuevoNombre.TabIndex = 0;
@@ -56,57 +56,64 @@
             // 
             // txtNombre
             // 
-            this.txtNombre.Location = new System.Drawing.Point(156, 47);
+            this.txtNombre.Location = new System.Drawing.Point(158, 7);
             this.txtNombre.Name = "txtNombre";
             this.txtNombre.Size = new System.Drawing.Size(156, 20);
             this.txtNombre.TabIndex = 1;
             // 
             // dgvFuncionalidadesDisponibles
             // 
+            this.dgvFuncionalidadesDisponibles.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
             this.dgvFuncionalidadesDisponibles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvFuncionalidadesDisponibles.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.NombreDeFuncionalidad});
-            this.dgvFuncionalidadesDisponibles.Location = new System.Drawing.Point(18, 90);
+            this.dgvFuncionalidadesDisponibles.Location = new System.Drawing.Point(20, 50);
             this.dgvFuncionalidadesDisponibles.Name = "dgvFuncionalidadesDisponibles";
-            this.dgvFuncionalidadesDisponibles.Size = new System.Drawing.Size(294, 307);
+            this.dgvFuncionalidadesDisponibles.ReadOnly = true;
+            this.dgvFuncionalidadesDisponibles.Size = new System.Drawing.Size(355, 395);
             this.dgvFuncionalidadesDisponibles.TabIndex = 2;
             // 
             // NombreDeFuncionalidad
             // 
             this.NombreDeFuncionalidad.HeaderText = "Funcionalidad";
             this.NombreDeFuncionalidad.Name = "NombreDeFuncionalidad";
+            this.NombreDeFuncionalidad.ReadOnly = true;
             // 
             // dgvFuncionalidadesActuales
             // 
+            this.dgvFuncionalidadesActuales.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
             this.dgvFuncionalidadesActuales.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvFuncionalidadesActuales.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.dataGridViewTextBoxColumn1});
-            this.dgvFuncionalidadesActuales.Location = new System.Drawing.Point(346, 90);
+            this.dgvFuncionalidadesActuales.Location = new System.Drawing.Point(460, 50);
             this.dgvFuncionalidadesActuales.Name = "dgvFuncionalidadesActuales";
-            this.dgvFuncionalidadesActuales.Size = new System.Drawing.Size(305, 307);
+            this.dgvFuncionalidadesActuales.ReadOnly = true;
+            this.dgvFuncionalidadesActuales.Size = new System.Drawing.Size(366, 395);
             this.dgvFuncionalidadesActuales.TabIndex = 3;
             // 
             // dataGridViewTextBoxColumn1
             // 
             this.dataGridViewTextBoxColumn1.HeaderText = "Funcionalidad";
             this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
             // 
             // lblRoles
             // 
             this.lblRoles.AutoSize = true;
+            this.lblRoles.Enabled = false;
             this.lblRoles.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
             this.lblRoles.ForeColor = System.Drawing.SystemColors.MenuHighlight;
-            this.lblRoles.Location = new System.Drawing.Point(15, 13);
+            this.lblRoles.Location = new System.Drawing.Point(354, 10);
             this.lblRoles.Name = "lblRoles";
-            this.lblRoles.Size = new System.Drawing.Size(47, 18);
+            this.lblRoles.Size = new System.Drawing.Size(117, 18);
             this.lblRoles.TabIndex = 4;
-            this.lblRoles.Text = "Roles";
+            this.lblRoles.Text = "Nombre Anterior";
             // 
             // btnQuitar
             // 
-            this.btnQuitar.Location = new System.Drawing.Point(213, 415);
+            this.btnQuitar.Location = new System.Drawing.Point(381, 166);
             this.btnQuitar.Name = "btnQuitar";
-            this.btnQuitar.Size = new System.Drawing.Size(175, 23);
+            this.btnQuitar.Size = new System.Drawing.Size(73, 23);
             this.btnQuitar.TabIndex = 6;
             this.btnQuitar.Text = "Quitar funcionalidad";
             this.btnQuitar.UseVisualStyleBackColor = true;
@@ -114,18 +121,18 @@
             // 
             // btnAgregar
             // 
-            this.btnAgregar.Location = new System.Drawing.Point(32, 415);
+            this.btnAgregar.Location = new System.Drawing.Point(381, 75);
             this.btnAgregar.Name = "btnAgregar";
-            this.btnAgregar.Size = new System.Drawing.Size(175, 23);
+            this.btnAgregar.Size = new System.Drawing.Size(73, 23);
             this.btnAgregar.TabIndex = 7;
-            this.btnAgregar.Text = "Agregar funcionalidad";
+            this.btnAgregar.Text = "Agregar";
             this.btnAgregar.UseVisualStyleBackColor = true;
             this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
             // 
             // lblFuncionalidadesTotales
             // 
             this.lblFuncionalidadesTotales.AutoSize = true;
-            this.lblFuncionalidadesTotales.Location = new System.Drawing.Point(15, 74);
+            this.lblFuncionalidadesTotales.Location = new System.Drawing.Point(17, 34);
             this.lblFuncionalidadesTotales.Name = "lblFuncionalidadesTotales";
             this.lblFuncionalidadesTotales.Size = new System.Drawing.Size(136, 13);
             this.lblFuncionalidadesTotales.TabIndex = 8;
@@ -134,39 +141,38 @@
             // lblFuncionalidadesActuales
             // 
             this.lblFuncionalidadesActuales.AutoSize = true;
-            this.lblFuncionalidadesActuales.Location = new System.Drawing.Point(343, 74);
+            this.lblFuncionalidadesActuales.Location = new System.Drawing.Point(520, 34);
             this.lblFuncionalidadesActuales.Name = "lblFuncionalidadesActuales";
             this.lblFuncionalidadesActuales.Size = new System.Drawing.Size(128, 13);
             this.lblFuncionalidadesActuales.TabIndex = 9;
             this.lblFuncionalidadesActuales.Text = "Funcionalidades Actuales";
             // 
-            // btnModificar
-            // 
-            this.btnModificar.Location = new System.Drawing.Point(394, 415);
-            this.btnModificar.Name = "btnModificar";
-            this.btnModificar.Size = new System.Drawing.Size(175, 23);
-            this.btnModificar.TabIndex = 10;
-            this.btnModificar.Text = "Modificar funcionalidad";
-            this.btnModificar.UseVisualStyleBackColor = true;
-            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
-            // 
             // btnVolver
             // 
-            this.btnVolver.Location = new System.Drawing.Point(610, 454);
+            this.btnVolver.Location = new System.Drawing.Point(609, 451);
             this.btnVolver.Name = "btnVolver";
-            this.btnVolver.Size = new System.Drawing.Size(75, 23);
+            this.btnVolver.Size = new System.Drawing.Size(192, 36);
             this.btnVolver.TabIndex = 11;
             this.btnVolver.Text = "Volver";
             this.btnVolver.UseVisualStyleBackColor = true;
             this.btnVolver.Click += new System.EventHandler(this.btnVolver_Click);
             // 
+            // btnModficar
+            // 
+            this.btnModficar.Location = new System.Drawing.Point(78, 451);
+            this.btnModficar.Name = "btnModficar";
+            this.btnModficar.Size = new System.Drawing.Size(192, 36);
+            this.btnModficar.TabIndex = 12;
+            this.btnModficar.Text = "Modificar";
+            this.btnModficar.UseVisualStyleBackColor = true;
+            // 
             // frmModificarRoles
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(697, 489);
+            this.ClientSize = new System.Drawing.Size(838, 489);
+            this.Controls.Add(this.btnModficar);
             this.Controls.Add(this.btnVolver);
-            this.Controls.Add(this.btnModificar);
             this.Controls.Add(this.lblFuncionalidadesActuales);
             this.Controls.Add(this.lblFuncionalidadesTotales);
             this.Controls.Add(this.btnAgregar);
@@ -200,7 +206,7 @@
         private System.Windows.Forms.Button btnAgregar;
         private System.Windows.Forms.Label lblFuncionalidadesTotales;
         private System.Windows.Forms.Label lblFuncionalidadesActuales;
-        private System.Windows.Forms.Button btnModificar;
         private System.Windows.Forms.Button btnVolver;
+        private System.Windows.Forms.Button btnModficar;
     }
 }
